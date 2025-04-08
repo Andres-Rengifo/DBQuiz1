c354207b62f8454073ee06287bff3813ab07c87a
Andres Rengifo <andrew.rengifo.m@gmail.com>
2025-04-08 23:24:03 +0100

    Q3

--- src/main/java/server/MatrixServiceImpl.java
+++ src/main/java/server/MatrixServiceImpl.java
@@ -10,8 +10,8 @@ public class MatrixServiceImpl extends MatrixServiceImplBase {
 	@Override
 	public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply) {
 		System.out.println("Request received from client:\n" + request);
-		int C00 = request.getA00() + request.getB00();
-		int C01 = request.getA01() + request.getB01() * 2;
+		int C00 = request.getA00() + request.getB00() * 2;
+		int C01 = request.getA01() + request.getB01();
 		int C10 = request.getA10() + request.getB10();
 		int C11 = request.getA11() + request.getB11();
 		MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
